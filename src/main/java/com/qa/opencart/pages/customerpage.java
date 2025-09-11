94e212768b0b3cb66814934017ed6a1101ec70b5
ravikiranautomationframework <ravikiranrgs8@gmail.com>
2025-09-11 12:57:01 +0530

    added the customerpage

---
+++ src/main/java/com/qa/opencart/pages/customerpage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class customerpage {
+
+	public static void main(String[] args) {
+		 
+		
+		int i = 20;
+		int x = 30;
+		
+		System.out.println(i+x);
+
+	}
+
+}
