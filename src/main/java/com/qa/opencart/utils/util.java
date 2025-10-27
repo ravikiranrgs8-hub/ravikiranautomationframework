d9005e4ab3de9810ce8e8d27cde86e173aded70c
kiranautomationframework <ravikiranrgs8@gmail.com>
2025-10-27 17:50:01 +0530

    added util page

---
+++ src/main/java/com/qa/opencart/utils/util.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.utils;
+
+public class util {
+
+	public static void main(String[] args) {
+		 int a = 26, b =36;
+		 
+		 int sum = a+b;
+		 
+		 System.out.println("The total sum of 2 number is: " + sum);
+		 
+
+	}
+
+}
